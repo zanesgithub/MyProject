10f28dae084b2a3ba1fe198542a2334828ebf1d8
zanesgithub <zanerandellzane@gmail.com>
2018-05-30 22:21:33 +1200

    prototype functionality

--- app/src/main/java/com/example/zanespc/myproject/MainActivity.java
+++ app/src/main/java/com/example/zanespc/myproject/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.zanespc.myproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -21,12 +22,71 @@ public class MainActivity extends Activity {
         Button button = (Button) findViewById(R.id.selectButton);
         button.setOnClickListener(new View.OnClickListener() {
             CheckBox checkBoxThanos = (CheckBox)findViewById(R.id.checkBoxThanos);
+            CheckBox checkBoxNaruto = (CheckBox)findViewById(R.id.checkBoxNaruto);
+            CheckBox checkBoxZed = (CheckBox)findViewById(R.id.checkBoxZed);
+            CheckBox checkBoxMewtwo = (CheckBox)findViewById(R.id.checkBoxMewtwo);
+            CheckBox checkBoxBobby = (CheckBox)findViewById(R.id.checkBoxBobby);
+            CheckBox checkBoxBears = (CheckBox)findViewById(R.id.checkBoxBears);
+            CheckBox checkBoxSasuke = (CheckBox)findViewById(R.id.checkBoxSasuke);
+            CheckBox checkBoxCaptainAmerica = (CheckBox)findViewById(R.id.checkBoxCaptainAmerica);
+            CheckBox checkBoxGoku = (CheckBox)findViewById(R.id.checkBoxGoku);
 
+            Intent i = new Intent(MainActivity.this, FightActivity.class);
+            Bundle extras = new Bundle();
+
+            public String[] F = {"1", "2", "3"};
             int m = 0;
-            public void onClick(View v) {
-                if(checkBoxThanos.isChecked())
-                    Log.d("myTag", "This is my message");
 
+            public void onClick(View v) {
+                if(checkBoxThanos.isChecked()) {
+                    extras.putString(F[m], "thanos");
+                    m++;
+                }
+
+                if(checkBoxNaruto.isChecked()){
+                    extras.putString(F[m], "naruto");
+                    m++;
+                }
+
+                if(checkBoxZed.isChecked()){
+                    extras.putString(F[m], "zed");
+                    m++;
+                }
+
+                if(checkBoxMewtwo.isChecked()){
+                    extras.putString(F[m], "mewtwo");
+                    m++;
+                }
+
+                if(checkBoxBears.isChecked()){
+                    extras.putString(F[m], "four_bears");
+                    m++;
+                }
+
+                if(checkBoxSasuke.isChecked()){
+                    extras.putString(F[m], "sasuke");
+                    m++;
+                }
+
+                if(checkBoxCaptainAmerica.isChecked()){
+                    extras.putString(F[m], "captain_america");
+                    m++;
+                }
+
+                if(checkBoxBobby.isChecked()){
+                    extras.putString(F[m], "bobby_shmurda");
+                    m++;
+                }
+
+                if(checkBoxGoku.isChecked()){
+                    extras.putString(F[m], "goku");
+                    m++;
+                }
+
+                if(m == 3) {
+                    i.putExtras(extras);
+                    startActivity(i);
+                }
             }
         });
 
--- app/src/main/java/com/example/zanespc/myproject/MyClass.java
+++
@@ -1,8 +0,0 @@
-package com.example.zanespc.myproject;
-
-/**
- * Created by Zane's PC on 12/05/2018.
- */
-
-public class MyClass {
-}
